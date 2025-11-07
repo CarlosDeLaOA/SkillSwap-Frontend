185b829c619ea4c5a1bbb35f295ea30a97c44a6a
Camila Morales <163371126+Cmoralescen@users.noreply.github.com>
2025-11-07 14:11:57 -0600

    Basic dashboard structure

---
+++ src/app/components/components/balance-dashboard/balance-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './balance-dashboard.component.html',
+  styleUrl: './balance-dashboard.component.scss'
+})
+export class BalanceDashboardComponent {
+
+}
---
+++ src/app/components/components/skills-progress/skills-progress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-progress',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills-progress.component.html',
+  styleUrl: './skills-progress.component.scss'
+})
+export class SkillsProgressComponent {
+
+}
